60dd5799432e68dd2d34142b8432bd369df38b15
tclutin <tclutin01@gmail.com>
2024-05-13 02:13:53 +0500

    Initial commit

---
+++ src/main/java/com/example/messagingrabbitmq/MessagingRabbitmqApplication.java
@@ -0,0 +1,52 @@
+package com.example.messagingrabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class MessagingRabbitmqApplication {
+
+	protected static final String fanoutExchangeName = "spring-boot-exchange";
+	protected static final String queueName = "spring-boot" + UUID.randomUUID();
+
+	@Bean
+	protected Queue queue() {
+		return new Queue(queueName, false, true,true);
+	}
+
+	@Bean
+	protected FanoutExchange exchange() {
+		return new FanoutExchange(fanoutExchangeName);
+	}
+
+	@Bean
+	protected Binding binding(Queue queue, FanoutExchange exchange) {
+		return BindingBuilder.bind(queue).to(exchange);
+	}
+
+	@Bean
+	protected SimpleMessageListenerContainer container(ConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter) {
+		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.setQueueNames(queueName);
+		container.setMessageListener(listenerAdapter);
+		return container;
+	}
+
+	@Bean
+	protected MessageListenerAdapter listenerAdapter(Receiver receiver) {
+		return new MessageListenerAdapter(receiver, "receiveMessage");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(MessagingRabbitmqApplication.class, args).close();
+	}
+
+}
---
+++ src/main/java/com/example/messagingrabbitmq/Runner.java
@@ -0,0 +1,33 @@
+package com.example.messagingrabbitmq;
+
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final Receiver receiver;
+
+	public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
+		this.receiver = receiver;
+		this.rabbitTemplate = rabbitTemplate;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			System.out.print("Enter message to send: ");
+			String message = scanner.nextLine();
+			rabbitTemplate.convertAndSend(MessagingRabbitmqApplication.fanoutExchangeName, "", message);
+			receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+		}
+	}
+
+}
